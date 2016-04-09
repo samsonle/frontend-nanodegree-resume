0b67c4b6f508b1495d9c69a441c1d26cb0bec574
samle75 <samle75@ad.liu.se>
2016-04-09 14:07:34 +0200

    Got things to show up on index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,18 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+ This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+    name: "Sam",
+    role: "Web Developer",
+    contacts: {
+        mobile: "XXX-XXXXXXX",
+        email: "samsonle@live.com",
+        github: "SamSonLe",
+        twitter: "SamSonLe",
+        location: "Sweden"
+    },
+    welcomeMessage: "Welcome to this homepage",
+    picture_URL: "./images/fry.jpg",
+    skills: ["none so far"]
+};
